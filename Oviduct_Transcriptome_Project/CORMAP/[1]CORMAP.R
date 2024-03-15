d7b25c9df9978676250169038433b0ca3e767830
Kenneth Lai <kennethlai@Kenneths-MacBook-Pro-7.local>
2024-03-15 16:36:20 -0700

    updating all code for final analyses / figures - 3/15/24

--- Oviduct_Transcriptome_Project/CORMAP/[1]CORMAP.R
+++ Oviduct_Transcriptome_Project/CORMAP/[1]CORMAP.R
@@ -21,13 +21,19 @@ library(grid)
 library(gplots)
 #install.packages("ComplexHeatmap")
 library("ComplexHeatmap")
-
+library(devtools)
+library(scales)
+source("/Users/kennethlai/desktop/HAWKINS/FAANG/RNAseq/colorpalette/RHodor_colors/R/RHodor_palettes.R")
 
 #IMPORT DATA 
-  DESeqdata=read.csv("/Users/kennethlai/desktop/HAWKINS/FAANg/RNAseq/DE/[global]DESEQ-results3.csv", header = T, na.strings="NA")
-  phenodata= read.table("/Users/kennethlai/desktop/HAWKINS/FAANg/RNAseq/QC/data/pheno_2(edit).txt", header=T)
-  phenodatareproductive= subset(phenodata, System == "Reproductive")
-
+  DESeqdata=read.csv("/Users/kennethlai/desktop/HAWKINS/FAANg/RNAseq/DE/[global]DESEQ-results7.csv", header = T, na.strings="NA")
+  #phenodata= read.table("/Users/kennethlai/desktop/HAWKINS/FAANg/RNAseq/QC/data/pheno_2(edit).txt", header=T)
+  #phenodatareproductive= subset(phenodata, System == "Reproductive")
+  
+  #1/23/24 : Andressa wants to rename samples w/ tissue names in figure w/ the newly abbreviated sample names in this updated file
+  phenodata_updated=read.table("/Users/kennethlai/Desktop/HAWKINS/FAANG/RNAseq/CORMAP/pheno_085_final.txt", header=T)
+  phenodatareproductive_updated= subset(phenodata_updated, System == "Reproductive")
+  
 #Making DESeqdata more compatible 
 #1a. Subsetting columns of interest (getting rid of stats info) 
   column_numbers <- c(2, 9, 10, 11, 12, 13, 14, 15, 16)
@@ -40,10 +46,18 @@ library("ComplexHeatmap")
   DESeqdata.filtered  <- DESeqdata.filtered [, -1]
 #DON'T NEED TO NORMALIZE? I skipped pre-PCA normalization bc DESeqdata = DESEQ2 results that were filtered by logFC >=1 and merged with already-normalized count data (ddsreproductivee)
 
-#3a. set this DESEQ dataframe as a matrix 
+#3a. Set this DESEQ dataframe as a matrix 
   DESeqdata.filtered = as.matrix (DESeqdata.filtered)
-#3b. rename all coltitles (sample names)
+#3b. rename all coltitles (sample names)--> match to Andressa's requested sample names instead 
   colnames(DESeqdata.filtered) <- sub("^Sample_", "", colnames(DESeqdata.filtered))
+  colnames(DESeqdata.filtered) <- sub("ep", "", colnames(DESeqdata.filtered))
+  
+#tissue names replacement? 
+  #colnames(DESeqdata.filtered)[grepl("^od1", colnames(DESeqdata.filtered))] <- "Magnum(od1)"
+  #colnames(DESeqdata.filtered)[grepl("^od2", colnames(DESeqdata.filtered))] <- "Isthmus(od2)"
+  #colnames(DESeqdata.filtered)[grepl("^od3", colnames(DESeqdata.filtered))] <- "Shell.Gland(od3)"
+  #colnames(DESeqdata.filtered)[grepl("^ovary", colnames(DESeqdata.filtered))] <- "Ovary"
+  
   
 #CREATE CORRELATION MATRIX 
 cor_matrix <- cor(DESeqdata.filtered)
@@ -52,9 +66,27 @@ cor_matrix <- cor(DESeqdata.filtered)
 
 #COR MATRIX --> HEATMAP 
 
+#DEFINING COLOR PALETTE wes anderson
+#pal <- wes_palette("Zissou1", 100, type = "continuous")
 
-##VERSION #2 
+#CREATING ANNOTATIONS (TISSUE - LEGEND)
 
+  # Create a sample grouping vector
+tissue_groups <- c("Magnum","Magnum", "Isthmus","Isthmus","Shell_Gland","Shell_Gland","Ovary","Ovary")
+
+  # Convert the grouping vector to a factor
+tissue_groups_factor <- factor(tissue_groups)
+
+  # Create a color palette for groups
+tissue_groups_colors <- c("Magnum" = "#F87008", "Isthmus" = "#27DAD8", "Shell_Gland" = "#22E11D", "Ovary" = "#F215EE")
+
+  # Create the heatmap annotation based on sample groups
+TissueGroupAnnot <- HeatmapAnnotation(
+  df = data.frame(Tissue = tissue_groups_factor),
+  col = list(Tissue = tissue_groups_colors)
+)
+
+#PLOTTING HEATMAP  
 corhmv2=Heatmap(
   cor_matrix,
   show_column_names = TRUE,
@@ -65,36 +97,18 @@ corhmv2=Heatmap(
   row_title = "Samples",
   row_names_gp = gpar(fontsize = 9, fontface = "bold"),
   column_names_gp = gpar(fontsize = 9, fontface = "bold"),
-  col = colorRampPalette(c("deepskyblue3", "black","red2"))(100),
+  #col = colorRampPalette(c("deepskyblue3", "black","red2"))(100),
+  col = hodor_pal("soho") (10),
   heatmap_legend_param = list(
     title_gp = gpar(fontsize = 10, fontface = "bold"),
     labels_gp = gpar(fontsize = 6)
   ),
   row_names_side = "right",
   show_row_dend = TRUE,
-  show_column_dend = FALSE)
-  
+  show_column_dend = FALSE,
+  top_annotation=TissueGroupAnnot)
 
 #SAVING AS PDFs
-pdf("CorrelationHeatmap(1).pdf", width = 10, height = 8)
+pdf("CorrelationHeatmap(6).pdf", width = 10, height = 7)
 print(corhmv2)
 dev.off()
-
-
-
-
-##VERSION #1 
-
-# Set the color palette, using white for self-correlations
-#my_palette <- colorRampPalette(c("deepskyblue3", "white", "red2"))(100)
-#my_palette[51] <- "white"  # Set color for self-correlations to white
-
-# Create the heatmap with adjusted color scale
-#corhmv1=heatmap.2(cor_matrix,
-                  #col = my_palette,
-                 #main = "Correlation Map of DESeq2 Normalized Counts",
-                 # xlab = "Samples",
-                 # ylab = "Samples",
-                 # margin = c(10, 10))
-
-
--- Oviduct_Transcriptome_Project/[1]DESeqPCA.R
+++ Oviduct_Transcriptome_Project/[1]DESeqPCA.R
@@ -17,7 +17,7 @@ library(factoextra)
 library(svglite)
 
 #IMPORT DATA 
-DESeqdata=read.csv("/Users/kennethlai/desktop/HAWKINS/FAANg/RNAseq/DE/[global]DESEQ-results3.csv", header = T, na.strings="NA")
+DESeqdata=read.csv("/Users/kennethlai/desktop/HAWKINS/FAANg/RNAseq/DE/[global]DESEQ-results7.csv", header = T, na.strings="NA")
 phenodata= read.table("/Users/kennethlai/desktop/HAWKINS/FAANg/RNAseq/QC/data/pheno_2(edit).txt", header=T)
 phenodatareproductive= subset(phenodata, System == "Reproductive")
 
@@ -33,21 +33,29 @@ phenodatareproductive= subset(phenodata, System == "Reproductive")
     DESeqdata.filtered  <- DESeqdata.filtered [, -1]
     #DON'T NEED TO NORMALIZE? I skipped pre-PCA normalization bc DESeqdata = DESEQ2 results that were fileterd by logFC >=1 and merged with already-normalized count data (ddsreproductivee)
     
+    
+    #Sample_od1_rep4 Sample_od1_rep5 Sample_od2_rep2 Sample_od2_rep6 Sample_od3_rep1 Sample_od3_rep5 Sample_ovary_rep5 Sample_ovary_rep7
+
+    
 #Perform PCA on DESeq2 results 
   #PCA-alltissues 
-pdf("PCA-DE_reproductive_tissues.pdf", width = 8, height = 6)
+pdf("[6]PCA-DE_reproductive_tissues.pdf", width = 8, height = 6)
 par(mar = c(8, 4, 4, 4))
 par(cex = 0.8)
 
 # Define the number of colors you want
 nb.cols <- 20
-mycolors <- colorRampPalette(brewer.pal(8, "Set1"))(nb.cols)
+#mycolors <- colorRampPalette(brewer.pal(8, "Set1"))(nb.cols)
 pca.counts2 = prcomp(t(DESeqdata.filtered), scale=F)
+#Set custom colors for each tissue
+custom_colors <- c("Magnum.od1" = "#F87008", "Isthmus.od2" = "#27DAD8", "Shell_Gland.od3" = "#22E11D", "Ovary" = "#F215EE")
+#Create a vector of colors based on the tissue information in phenodatareproductive$Tissue
+color_vector <- custom_colors[as.character(phenodatareproductive$Tissue)]
 fviz_pca_ind(pca.counts2,
              pointsize=2,
              geom.ind = ("point"), # show points only (nbut not "text")
              habillage = phenodatareproductive$Tissue, # color by groups
-             palette = mycolors,
+             palette = color_vector,
              legend.title = "Tissue",
              mean.point = FALSE
 )
